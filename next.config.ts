ea8b630d008064d56b1d46adf81c239ab8f56a1a
yatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2026-08-16 03:43:09 +0530

    CICD

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/portfolio-web",
+  assetPrefix: "/portfolio-web/",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
